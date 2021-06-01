8cd0a8d62d29c02942c4461705d302adf5a23d87
Yur4ik1234 <gajdaryura@gmail.com>
2021-06-01 10:24:51 +0300

    Ispar
    Inspar

---
+++ app/src/main/java/com/example/first_app/Account.java
@@ -0,0 +1,40 @@
+package com.example.first_app;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.concurrent.Executor;
+
+
+public class Account extends Fragment {
+
+    public Account() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       View v= inflater.inflate(R.layout.fragment_account, container, false);
+
+
+
+       return v;
+    }
+}
---
+++ app/src/main/java/com/example/first_app/Settings.java
@@ -0,0 +1,31 @@
+package com.example.first_app;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Settings extends Fragment {
+
+
+    public Settings() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_settings, container, false);
+
+        return view;
+    }
+
+}
